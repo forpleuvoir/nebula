e4d1c7dfbc4a1831aad8c3ac7e791057ffdc6117
forpleuvoir <695801070@qq.com>
2022-12-07 00:34:45 +0800

    构建脚本优化

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,7 +1,13 @@
+val yamlVersion = "1.33"
+val dom4jVersion = "2.1.3"
+val guavaVersion = "31.0.1-jre"
+val gsonVersion = "2.8.9"
+val tomljVersion = "1.1.0"
+
 dependencies {
-	implementation("org.yaml:snakeyaml:1.33")
-	implementation("org.dom4j:dom4j:2.1.3")
-	implementation("com.google.guava:guava:31.0.1-jre")
-	implementation("com.google.code.gson:gson:2.8.9")
-	implementation("org.tomlj:tomlj:1.1.0")
+	implementation("org.yaml:snakeyaml:$yamlVersion")
+	implementation("org.dom4j:dom4j:$dom4jVersion")
+	implementation("com.google.guava:guava:$guavaVersion")
+	implementation("com.google.code.gson:gson:$gsonVersion")
+	implementation("org.tomlj:tomlj:$tomljVersion")
 }
