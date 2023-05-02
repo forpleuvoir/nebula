de937e273869c11b2fc6fb67a76f81b3c67609e2
forpleuvoir <695801070@qq.com>
2023-05-02 20:47:41 +0800

    颜色实现调整 0.2.2e

--- common/src/main/kotlin/com/forpleuvoir/nebula/common/color/ARGBColor.kt
+++ common/src/main/kotlin/com/forpleuvoir/nebula/common/color/ARGBColor.kt
@@ -1,27 +1,11 @@
 package com.forpleuvoir.nebula.common.color
 
-interface ARGBColor {
+interface ARGBColor : RGBColor {
 
     val argb: Int
-
-    val rgb: Int
 
     val alpha: Int
 
     val alphaF: Float
-
-    val red: Int
-
-    val redF: Float
-
-    val green: Int
-
-    val greenF: Float
-
-    val blue: Int
-
-    val blueF: Float
-
-    val hexStr: String
 
 }
