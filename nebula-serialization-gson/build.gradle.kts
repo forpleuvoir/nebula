ef09cde9f592b7b6edd68b872a228b522f90f883
forpleuvoir <695801070@qq.com>
2023-12-04 14:50:45 +0800

    0.2.6f 颜色不透明度方法opacity(opacity:Int)计算不透明度值方法错误修正 构建脚本打包修改:nebula分类器,只会打包nebula 项目中的代码

--- nebula-serialization-gson/build.gradle.kts
+++ nebula-serialization-gson/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+
 val gsonVersion = "2.10"
 
 dependencies {
@@ -5,3 +8,11 @@ dependencies {
     api("com.google.code.gson:gson:$gsonVersion")
 }
 
+tasks.withType<ShadowJar>().configureEach {
+    archivesName.set("${project.name}-nebula")
+    archiveClassifier.set("nebula")
+    dependencies {
+        include(dependency("moe.forpleuvoir:nebula-common"))
+        include(dependency("moe.forpleuvoir:nebula-serialization"))
+    }
+}
