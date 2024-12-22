9e1af33389800de04a23e5b29d8461c9944dbe58
forpleuvoir <695801070@qq.com>
2024-12-22 20:20:30 +0800

    移除脚本模块相关文件
    删除nebula-script模块的构建脚本和测试文件，移除未使用的代码以精简项目结构。这可能与模块下线或重构相关。

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ rootProject.name = "nebula"
 
 include(":nebula-common")
 include(":nebula-config")
-include(":nebula-script")
 include(":nebula-serialization")
 include(":nebula-serialization-gson")
 include(":nebula-event")
